bb9ec0fe9ec0883159288da4b52428a73325c83a
luofeng457 <luofeng457@163.com>
2019-10-24 23:21:13 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Ellipse from '../components/Ellipse';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import '../public/pages/index.css';
+
+const Index = () => (
+<div className="wrapper">
+    <h2>CSS Demos</h2>
+    <Ellipse />
+</div>
+)
+
+export default Index;
